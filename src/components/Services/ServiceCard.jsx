c5c992ab4ec460f2eaf6d041a05d154586674eae
Josu Huelmo <josu.huelmo@gmail.com>
2026-03-15 17:01:17 +0100

    modified carousel and home elements

--- src/components/Services/ServiceCard.jsx
+++ src/components/Services/ServiceCard.jsx
@@ -1,107 +1,108 @@
-import { useState } from 'react';
-import { motion } from 'motion/react';
-import { Check } from 'lucide-react';
+import { useState } from "react";
+import { motion } from "motion/react";
+import { Check } from "lucide-react";
 
 const ServiceCard = ({ service }) => {
-  const [isHovered, setIsHovered] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
-  return (
-    <motion.div
-      className="relative rounded-3xl o cursor-pointer border border-gray-200"
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      initial={{ opacity: 0, y: 20 }}
-      whileInView={{ opacity: 1, y: 0 }}
-      viewport={{ once: true }}
-      transition={{ duration: 0.6 }}
-    >
-      <motion.div
-        className="absolute inset-0 bg-primary rounded-3xl"
-        animate={{ opacity: isHovered ? 1 : 0 }}
-        transition={{ duration: 0.4 }}
-      />
+    return (
+        <motion.div
+            className="relative rounded-3xl o cursor-pointer border border-gray-200"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+            initial={{ opacity: 0, y: 20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 0.6 }}
+        >
+            <motion.div
+                className="absolute inset-0 bg-primary rounded-3xl"
+                animate={{ opacity: isHovered ? 1 : 0 }}
+                transition={{ duration: 0.4 }}
+            />
 
-      {/* Image on hover */}
-      <motion.div
-        className="absolute right-10 top-[-10%]  rotate-20 aspect-square z-100 w-85 rounded-3xl overflow-hidden"
-        animate={{ 
-          opacity: isHovered ? 1 : 0,
-          x: isHovered ? 25 : -25,
-        }}
-        transition={{ duration: 0.4, ease: 'easeOut' }}
-      >
-        <img 
-          src={service.image} 
-          alt={service.title}
-          className="h-full w-full object-cover"
-        />
-      </motion.div>
-
-      {/* Content */}
-      <div className="relative p-8 lg:p-12 flex flex-col justify-between">
-        <div className="grid grid-cols-3 gap-8">
-          {/* Number and Title */}
-          <div className="flex items-start justify-center gap-8">
-            <motion.span 
-              className="text-md font-light"
-              animate={{ 
-                color: isHovered ? '#ffffff' : '#2d4453'
-              }}
-              transition={{ duration: 0.3 }}
-            >
-              {service.id}
-            </motion.span>
-            
-            <motion.h3 
-              className="text-xl lg:text-2xl font-bold font-headings"
-              animate={{ 
-                color: isHovered ? '#ffffff' : '#072957'
-              }}
-              transition={{ duration: 0.3 }}
-            >
-              {service.title}
-            </motion.h3>
-          </div>
-
-
-          {/* Items List */}
-          <div className="space-y-3 max-w-md">
-            {/* Description */}
-            <motion.p 
-              className="text-md overflow-hidden [display:-webkit-box] [-webkit-line-clamp:3] [-webkit-box-orient:vertical]"
-                animate={{ 
-                color: isHovered ? '#e5e5e5' : '#2d4453'
+            {/* Image on hover */}
+            <motion.div
+                className="absolute right-10 top-[-10%] aspect-square z-100 w-85 rounded-3xl overflow-hidden"
+                animate={{
+                    opacity: isHovered ? 1 : 0,
+                    x: isHovered ? 25 : -25,
                 }}
-                transition={{ duration: 0.3 }}
+                transition={{ duration: 0.4, ease: "easeOut" }}
             >
-                {service.description}
-            </motion.p>
-            {service.items.map((item, index) => (
-              <motion.div 
-                key={index}
-                className="flex items-center gap-3"
-                animate={{ 
-                  color: isHovered ? '#ffffff' : '#2d4453'
-                }}
-                transition={{ duration: 0.3 }}
-              >
-                <motion.div
-                  animate={{
-                    backgroundColor: isHovered ? '#c9b27c' : '#2d4453'
-                  }}
-                  transition={{ duration: 0.3 }}
-                  className="rounded-full p-1"
-                >
-                  <Check className="w-4 h-4 text-white" />
-                </motion.div>
-                <span className="text-base">{item}</span>
-              </motion.div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </motion.div>
-  );
+                <img
+                    src={service.image}
+                    alt={service.title}
+                    className="h-full w-full object-cover"
+                />
+            </motion.div>
+
+            {/* Content */}
+            <div className="relative p-8 lg:p-12 flex flex-col justify-between">
+                <div className="grid grid-cols-3 gap-8">
+                    {/* Number and Title */}
+                    <div className="flex items-start justify-center gap-8">
+                        <motion.span
+                            className="text-md font-light"
+                            animate={{
+                                color: isHovered ? "#ffffff" : "#2d4453",
+                            }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            {service.id}
+                        </motion.span>
+
+                        <motion.h3
+                            className="text-xl lg:text-2xl font-bold font-headings"
+                            animate={{
+                                color: isHovered ? "#ffffff" : "#072957",
+                            }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            {service.title}
+                        </motion.h3>
+                    </div>
+
+                    {/* Items List */}
+                    <div className="space-y-3 max-w-md">
+                        {/* Description */}
+                        <motion.p
+                            className="text-md overflow-hidden [display:-webkit-box] [-webkit-line-clamp:3] [-webkit-box-orient:vertical]"
+                            animate={{
+                                color: isHovered ? "#e5e5e5" : "#2d4453",
+                            }}
+                            transition={{ duration: 0.3 }}
+                        >
+                            {service.description}
+                        </motion.p>
+                        {service.items.map((item, index) => (
+                            <motion.div
+                                key={index}
+                                className="flex items-center gap-3"
+                                animate={{
+                                    color: isHovered ? "#ffffff" : "#2d4453",
+                                }}
+                                transition={{ duration: 0.3 }}
+                            >
+                                <motion.div
+                                    animate={{
+                                        backgroundColor: isHovered
+                                            ? "#c9b27c"
+                                            : "#2d4453",
+                                    }}
+                                    transition={{ duration: 0.3 }}
+                                    className="rounded-full p-1"
+                                >
+                                    <Check className="w-4 h-4 text-white" />
+                                </motion.div>
+                                <span className="text-base">{item}</span>
+                            </motion.div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </motion.div>
+    );
 };
 
 export default ServiceCard;
--- src/components/Works.tsx
+++ src/components/Works.tsx
@@ -4,6 +4,15 @@ import service1 from "../assets/Services/Service-1.jpg";
 import service2 from "../assets/Services/Service-2.jpg";
 import service3 from "../assets/Services/Service-3.jpg";
 import { motion } from "motion/react";
+import { ArrowUpRight } from "lucide-react";
+import {
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from "./ui/carousel";
+import { Card, CardContent } from "./ui/card";
 
 const Works = () => {
     const workImages = [
@@ -14,44 +23,54 @@ const Works = () => {
         { src: service2.src, alt: "Trabajo 5" },
     ];
 
-    const duplicatedWorkImages = [...workImages, ...workImages];
-
     return (
-        <section id="works" className="w-full relative py-16">
-            <div className="text-center space-y-6">
-                <span className="block text-xl font-medium tracking-wider">
-                    Nuestra experiencia
-                </span>
-                <h2 className="text-7xl font-bold">QUE DICE DE NOSOTROS</h2>
-            </div>
-            <div className="mt-20 w-full">
-                <motion.ul
-                    className="flex w-full gap-16"
-                    animate={{
-                        x: ["0%", "-100%"],
-                        transition: {
-                            ease: "linear",
-                            duration: 40,
-                            repeat: Infinity,
-                        },
-                    }}
-                >
-                    {duplicatedWorkImages.map((image, index) => (
-                        <li
-                            key={`work-image-${index}`}
-                            className={`w-80 h-80 shrink-0 overflow-hidden rounded-3xl transition-transform duration-300
-                                ${index % 2 === 0 ? "rotate-6 hover:rotate-0" : "-rotate-6 hover:rotate-0"}
-                            `}
-                        >
-                            <img
-                                src={image.src}
-                                alt={image.alt}
-                                className={`h-full w-full object-cover rounded-3xl`}
-                                loading="lazy"
-                            />
-                        </li>
-                    ))}
-                </motion.ul>
+        <section className=" bg-neutral-50">
+            <div className="container-xl px-4 lg:px-8">
+                {/* Header */}
+                <div className="flex items-start justify-between mb-16">
+                    <h2 className="text-5xl lg:text-7xl font-bold font-headings text-primary">
+                        TRABAJOS
+                    </h2>
+
+                    <motion.button
+                        className="flex items-center gap-2 px-6 py-3 bg-accent text-accent-foreground rounded-full font-medium hover:bg-accent-dark transition-colors"
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                    >
+                        Ver Todos los Servicios
+                        <ArrowUpRight className="w-5 h-5" />
+                    </motion.button>
+                </div>
+
+                <div className="mt-20 w-full">
+                    <Carousel
+                        className="w-full"
+                        opts={{ overflow: true, align: "start" }}
+                    >
+                        <CarouselContent>
+                            {workImages.map((image, index) => (
+                                <CarouselItem
+                                    className="md:basis-1/2 lg:basis-1/3"
+                                    key={index}
+                                >
+                                    <div className="p-1">
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-6">
+                                                <span className="text-4xl font-semibold">
+                                                    {image.alt}
+                                                </span>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <div className="mt-6 flex justify-end gap-3">
+                            <CarouselPrevious className="static! translate-x-0! translate-y-0!" />
+                            <CarouselNext className="static! translate-x-0! translate-y-0!" />
+                        </div>
+                    </Carousel>
+                </div>
             </div>
         </section>
     );
